f30c8f869f7f737af7166f583aedad144e113d05
encrypter-harsh <harshverma012005@gmail.com>
2024-02-05 20:27:25 +0530

    modified:   linklist.c

--- linklist.c
+++ linklist.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include <stdlib.h>
 typedef struct node{
     int info;
     struct node *link;
-};
+} node ;
 void main(){
-    printf("LINKLIST");
+    node *n1=(node*)malloc(sizeof(node));
+    scanf("%d",&n1->info);
+
 }
